642062cc11ff2ac1830528d9663b5ad5105378ae
arpitaagrawal <arpita@Arpitas-MacBook-Pro.local>
2016-11-12 19:55:07 -0800

    updated distance metric files

--- src/clusters/get_distance_metric_col.py
+++ src/clusters/get_distance_metric_col.py
@@ -1,23 +1,31 @@
 import cPickle as pickle
 import numpy as np
 from  simple_expansion.simple_expansion_feature import users
-# from clusters import user_similarity_clusters
+
+"""
+
+    For this file to execute properly, copy all the user distance metric files (8 files for each metric in total)
+    -user_hamming_similarity_metric*.p,  user_l2_similarity_metric*.p to the folder var specified path
+    and call get_distance_metric_for_user() method as directed in the init method below.
+    
+"""
+folder = "/Users/arpita/MLTrioData/MlTrio_lfs/"
  
 no_of_splits= 8
 
-fname= "user_hamming_similarity_metric"
+fname= folder + "user_hamming_similarity_metric"
 user_hamming_distance_metric  = pickle.load(open(fname+"_0.p", "rb") )
 for i in range(1, no_of_splits):
     user_hamming_distance_metric  = np.vstack((user_hamming_distance_metric, pickle.load(open(fname+"_"+str(i)+".p", "rb") )))
-print 'size of l2 distance metric', np.shape(user_hamming_distance_metric) 
+print 'size of hamming distance metric', np.shape(user_hamming_distance_metric) 
 
-# 
-# fname= "user_l2_similarity_metric"
+ 
+fname= folder + "user_l2_similarity_metric"
 user_l2_distance_metric = []
-#user_l2_distance_metric  = pickle.load(open(fname+"_1.p", "rb") )
-# for i in range(2, no_of_splits+1):
-#     user_l2_distance_metric  = np.vstack((user_l2_distance_metric, pickle.load(open(fname+"_"+str(i)+".p", "rb") )))
-# print 'size of l2 distance metric', np.shape(user_l2_distance_metric) 
+user_l2_distance_metric  = pickle.load(open(fname+"_0.p", "rb") )
+for i in range(1, no_of_splits):
+    user_l2_distance_metric  = np.vstack((user_l2_distance_metric, pickle.load(open(fname+"_"+str(i)+".p", "rb") )))
+print 'size of l2 distance metric', np.shape(user_l2_distance_metric) 
 
 
 def get_distance_metric_for_user(U1,U2, metric):
@@ -36,7 +44,7 @@ def get_distance_metric_for_user(U1,U2, metric):
     
 if __name__ == '__main__':
 
-    #print get_distance_metric_for_user("f37635bcd54355ced64ff62b113bc692", "4588a1df2461674252ff01c63b59171a", "l2")
+    print get_distance_metric_for_user("f37635bcd54355ced64ff62b113bc692", "4588a1df2461674252ff01c63b59171a", "l2")
     print get_distance_metric_for_user("f37635bcd54355ced64ff62b113bc692", "4588a1df2461674252ff01c63b59171a", "hamming")
     
     
--- src/clusters/get_ques_similarity.py
+++ src/clusters/get_ques_similarity.py
@@ -3,16 +3,26 @@ import cPickle as pickle
 from simple_expansion.simple_expansion_feature import questions
 
 
+"""
+
+    For this file to execute properly, copy all the question distance metric files (2 files in total)
+    -ques_l2_similarity_metric.p,  ques_hamming_similarity_metric.p to the folder var defined below
+    and call get_distance_metric_for_ques() method as directed in the init method below.
+    
+"""
+
+folder = "/Users/arpita/MLTrioData/MlTrio_lfs/"
+
 def get_distance_metric_for_ques(Q1,Q2, metric):
     '''
-        metric : expected values : l1 / hamming
+        metric : expected values : l2 / hamming
         Returns a scalar value : distance measure between two users
     '''
 
-    if metric == 'l1':
-        ques_distance_metric  = pickle.load(open('ques_l1_similarity_metric.p', "rb") )
+    if metric == 'l2':
+        ques_distance_metric  = pickle.load(open(folder+"ques_l2_similarity_metric.p", "rb") )
     else:
-        ques_distance_metric  = pickle.load(open('ques_hamming_similarity_metric.p', "rb"))
+        ques_distance_metric  = pickle.load(open(folder+'ques_hamming_similarity_metric.p', "rb"))
 
     q1_index = questions.keys().index(Q1)
     
@@ -26,5 +36,5 @@ if __name__ == '__main__':
 #     print len(cluster_user_dict), cluster_user_dict[-1]
 #     print len(users_cluster_dict), users_cluster_dict[users_cluster_dict.keys()[0]]
 #     print set(users_cluster_dict.values())
-    print get_distance_metric_for_ques("367edcb36424493a7cf80f70903a64cd", "58695dc8b407850d8a028cec784f535c", 'l1')
+    print get_distance_metric_for_ques("367edcb36424493a7cf80f70903a64cd", "58695dc8b407850d8a028cec784f535c", 'l2')
     print get_distance_metric_for_ques("367edcb36424493a7cf80f70903a64cd", "58695dc8b407850d8a028cec784f535c", 'hamming')

ca8eea6041e8c1bdf94462218ce7090c885b5d93
smadha <msharan@usc.edu>
2016-11-14 22:05:22 -0800

    Grid search on train data

--- src/models/train_deep.py
+++ src/models/train_deep.py
@@ -9,6 +9,8 @@ from keras.optimizers import SGD, RMSprop
 from keras.callbacks import EarlyStopping
 import cPickle as pickle
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 import theano
 theano.config.openmp = True
@@ -93,41 +95,64 @@ with open("model/train_config", 'wb') as pickle_file:
     pickle.dump(save_res, pickle_file, protocol=2)
 print "Dumped config"
 
-reg_coeff = 1e-05
+
 momentum = 0.99
 eStop = True
 sgd_Nesterov = True
 sgd_lr = 1e-5
-sgd_decay = 1e-05
-arch = [len(features[0]),1024,512,2]
 batch_size=50000
-nb_epoch=50
+nb_epoch=100
 verbose=True
 
-call_ES = EarlyStopping(monitor='val_acc', patience=6, verbose=1, mode='auto')
 
-# Generate Model
-model = genmodel(num_units=arch, reg_coeff=reg_coeff )
-# Compile Model
-sgd = SGD(lr=sgd_lr, decay=sgd_decay, momentum=momentum, 
-    nesterov=sgd_Nesterov)
+def run_NN(arch, reg_coeff, sgd_decay, class_weight_0, save=False):
+    features_tr, features_te,labels_tr, labels_te = train_test_split(features,labels, train_size = 0.85)
+    
+    call_ES = EarlyStopping(monitor='val_acc', patience=6, verbose=1, mode='auto')
+    
+    # Generate Model
+    model = genmodel(num_units=arch, reg_coeff=reg_coeff )
+    # Compile Model
+    sgd = SGD(lr=sgd_lr, decay=sgd_decay, momentum=momentum, 
+        nesterov=sgd_Nesterov)
+    
+    # sgd = RMSprop(lr=sgd_lr, rho=0.9, epsilon=1e-08, decay=sgd_decay)
+    
+    model.compile(loss='MSE', optimizer=sgd, 
+        metrics=['accuracy'])
+    # Train Model
+    if eStop:
+        model.fit(features_tr, labels_tr, nb_epoch=nb_epoch, batch_size=batch_size, 
+        verbose=verbose, callbacks=[call_ES], validation_split=0.1, 
+        validation_data=None, shuffle=True, class_weight={0:class_weight_0 , 1:1})
+    else:
+        model.fit(features_tr, labels_tr, nb_epoch=nb_epoch, batch_size=batch_size, 
+            verbose=verbose, class_weight={0: class_weight_0 , 1:1})
+    
+    labels_pred = model.predict(features_te)
+    classification_report(labels_te, labels_pred)
+    if save:
+        # Save model
+        model.save("model/model_deep.h5")
+        print("Saved model to disk")
+    
 
-# sgd = RMSprop(lr=sgd_lr, rho=0.9, epsilon=1e-08, decay=sgd_decay)
-
-model.compile(loss='MSE', optimizer=sgd, 
-    metrics=['accuracy'])
-# Train Model
-if eStop:
-    model.fit(features, labels, nb_epoch=nb_epoch, batch_size=batch_size, 
-    verbose=verbose, callbacks=[call_ES], validation_split=0.1, 
-    validation_data=None, shuffle=True)
-else:
-    model.fit(features, labels, nb_epoch=nb_epoch, batch_size=batch_size, 
-        verbose=verbose)
-
-# Save model
-model.save("model/model_deep.h5")
-print("Saved model to disk")
  
+arch_range = [[len(features[0]),1024,2], [len(features[0]),1024,512,2], [len(features[0]),1024,1024,512,2],[len(features[0]),1024,512,256,2]]
+reg_coeffs_range = [1e-7, 1e-6, 5e-6, 1e-5]
+sgd_decays_range = [1e-5, 5e-5, 1e-4]
+class_weight_0_range = [0.5,0.75, 0.9]
+
+#GRID SEARCH ON BEST PARAM
+for arch in arch_range:
+    for reg_coeff in reg_coeffs_range:
+        for sgd_decay in sgd_decays_range:
+            for class_weight_0 in class_weight_0_range:
+                run_NN(arch, reg_coeff, sgd_decay, class_weight_0)
+
+# arch = [len(features[0]),1024,512,2]
+# reg_coeff = 1e-05
+# sgd_decay = 1e-05
+# class_weight_0 = 0.5
  
 

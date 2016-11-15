408e689e7adb72e2384cd303cf88375f7d0a34be
smadha <msharan@usc.edu>
2016-11-14 22:48:02 -0800

    Fix print class, add param in result display

--- src/models/train_deep.py
+++ src/models/train_deep.py
@@ -14,7 +14,7 @@ from sklearn.metrics import classification_report
 
 import theano
 theano.config.openmp = True
-OMP_NUM_THREADS=10 
+OMP_NUM_THREADS=22 
 
 def normalize(X_tr):
     ''' Normalize training and test data features
@@ -129,14 +129,18 @@ def run_NN(arch, reg_coeff, sgd_decay, class_weight_0, save=False):
         model.fit(features_tr, labels_tr, nb_epoch=nb_epoch, batch_size=batch_size, 
             verbose=verbose, class_weight={0: class_weight_0 , 1:1})
     
-    labels_pred = model.predict(features_te)
+    labels_pred = model.predict_classes(features_te)
     y_true, y_pred = [ 0*l[0] + 1*l[1] for l in labels_te], [ 0*l[0] + 1*l[1] for l in labels_pred]
     
     print labels_te[0], labels_pred[0], y_true[0], y_pred[0]
     report = classification_report(y_true, y_pred)
+    print "arch, reg_coeff, sgd_decay, class_weight_0", arch, reg_coeff, sgd_decay, class_weight_0
     print report
     with open("results_nn.txt", "a") as f:
         f.write(report)
+        f.write("\n")
+        f.write(" ".join([str(s) for s in ["arch, reg_coeff, sgd_decay, class_weight_0", arch, reg_coeff, sgd_decay, class_weight_0]]))
+        f.write("\n")
         
     if save:
         # Save model

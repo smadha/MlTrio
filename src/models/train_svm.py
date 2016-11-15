cbbcdb59ad49d98838e5c48d51b0ff04747a9603
smadha <msharan@usc.edu>
2016-11-15 03:11:37 -0800

    loosening SVM

--- src/models/train_svm.py
+++ src/models/train_svm.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 import numpy as np
 import cPickle as pickle
+from sklearn.metrics import f1_score, make_scorer
 
 def normalize(X_tr):
     ''' Normalize training and test data features
@@ -22,9 +23,14 @@ def normalize(X_tr):
     X_tr = X_tr/X_sig
     return X_tr, X_mu, X_sig
 
+def score_f1_class1(ground_truth, predictions):
+    '''
+    Returns f1 score for class 1
+    '''
+    return f1_score(ground_truth, predictions, average='binary', pos_label=1)
 
 features = pickle.load( open("../feature_engg/feature/all_features.p", "rb") )
-labels = pickle.load( open("../feature_engg/feature/labels.p", "rb") )
+labels = [int(l) for l in pickle.load( open("../feature_engg/feature/labels.p", "rb") )]
 
 
 print len(features),len(features[0])
@@ -37,25 +43,29 @@ print col_deleted
 features = np.delete(features, col_deleted, axis=1)
 
 print len(features),len(features[0])
-print len(labels),len(labels[0])
+print len(labels),labels[0]
 
 features, X_mu, X_sig = normalize(features)
 
-gamma_ramge = [ 4**i for i in range(-7,0) ]
-C_range = [ 4**i for i in range(-3,6) ]
+gamma_ramge = [ 4**i for i in range(-3,0) ]
+C_range = [ 4**i for i in range(-1,6) ]
+class_weight_range = [ {0: class_weight_0 , 1:1} for class_weight_0 in [0.25,0.5,0.75, 0.9]]
+
+score = make_scorer(score_f1_class1, greater_is_better=True)
 
 # Number of folds in Cross validation
 CV_FOLDS = 3
 # Number of parallel jobs
-parallel = 6
+parallel = 14
 
 X_tr, X_te,y_tr, y_te = train_test_split(features,labels, train_size = 0.85)
 
 svr = SVC()
 
-parameters = [{ 'kernel':['rbf'], 'C':C_range, 'gamma':gamma_ramge}]
+parameters = [{ 'kernel':['rbf'], 'C':C_range, 'gamma':gamma_ramge,'class_weight':class_weight_range
+               ,'cache_size':[1000], 'tol':[1e-2]}]
 
-clf = GridSearchCV(svr, parameters, cv=CV_FOLDS, n_jobs = parallel, verbose=10000)
+clf = GridSearchCV(svr, parameters, cv=CV_FOLDS, n_jobs = parallel, verbose=1000, iid=False, scoring=score)
 
 clf.fit(X_tr,y_tr)
 
@@ -67,9 +77,11 @@ print("Grid scores on development set:")
 print()
 means = clf.cv_results_['mean_test_score']
 stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-    print("%0.3f (+/-%0.03f) for %r"
-          % (mean, std * 2, params))
+mean_fit_time = clf.cv_results_['mean_fit_time']
+
+for mean, std, params,time in zip(means, stds, clf.cv_results_['params'],mean_fit_time):
+    print("%0.3f (+/-%0.03f) for %r time-%0.3f"
+          % (mean, std * 2, params,time))
 print()
 
 print("Detailed classification report:")

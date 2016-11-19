a66d19e523d131b920cfbbdb8885ee451f284a3b
arpitaagrawal <arpitaag@usc.edu>
2016-11-18 22:01:42 -0800

    valid user based users

--- src/users_in_valid_test.py
+++ src/users_in_valid_test.py
@@ -5,17 +5,20 @@ users_va_te = set([])
 ## validate_nolabel test_nolabel
 
 def add_all_user(fname):
+    global users_va_te
     with open(fname,"r") as f:
-        f.readline().strip().split(",")
-        training_data = f.readline().strip().split(",")
+        #l = f.readline().strip().split("\t")
+        training_data = f.readline().strip().split("\t")
         while training_data and len(training_data) >= 2 :
             user_id = training_data[1]
-            users_va_te.add(user_id)
-            training_data = f.readline().strip().split(",")
+            if training_data[2] == "1":
+                users_va_te.add(user_id)
+            training_data = f.readline().strip().split("\t")
             
-    
+    print len(users_va_te)
 
 if __name__ == '__main__':
-    add_all_user("../bytecup2016data/test_nolabel.txt")
-    add_all_user("../bytecup2016data/validate_nolabel.txt")
+
+    add_all_user("../bytecup2016data/invited_info_train.txt")
+    #add_all_user("../bytecup2016data/validate_nolabel.txt")
     pickle.dump(list(users_va_te) , open("../bytecup2016data/users_va_te.p","wb"), protocol=2)
